2010fe9d55e06b136f46d250810ffe47a62ddfce
Patrick Marchwiak <pmarchwiak@gmail.com>
2014-01-27 00:03:50 -0800

    Add icon, fix drop, add clear

--- scripts.js
+++ scripts.js
@@ -4,7 +4,9 @@
 var spawn = require('child_process').spawn;
 
 function setImage(filePath, idx) {
+    $("#holder" + idx).empty();
     var img = new Image();
+    img.id = "img" + idx;
     img.src = filePath;
     img.width = 300;
     img.height = 300;
@@ -12,67 +14,107 @@ function setImage(filePath, idx) {
 }
 
 function createDiptych(img1, img2, orientation, borderColor) {
-    var prc = spawn('montage',  ['-geometry', '+0+0', '-border', '5', '-bordercolor',
+    console.log("Creating diptych....");
+    var prc = spawn('montage', ['-geometry', '+0+0', '-border', '5', '-bordercolor',
                                  borderColor, img1, img2, '/tmp/diptych.png']);
-    
-    //noinspection JSUnresolvedFunction
+
     prc.stdout.setEncoding('utf8');
     prc.stdout.on('data', function (data) {
         var str = data.toString()
         var lines = str.split(/(\r?\n)/g);
         console.log(lines.join(""));
     });
-    
+
     prc.on('close', function (code) {
         console.log('process exit code ' + code);
+
     });
 }
 
-
-
 $(document).ready(function () {
     console.log("ready!");
-    var holder = document.querySelectorAll('div[id^=holder]');
-holder.ondragover = function () { this.className = 'hover'; return false; };
-holder.ondragend = function () { this.className = ''; return false; };
-holder.ondrop = function (e) {
-    e.preventDefault();
-
-    for (var i = 0; i < e.dataTransfer.files.length; ++i) {
-
-        console.log(e.dataTransfer.files[i].path);
-    }
-    return false;
-};
+    var holders = $(".holder");
     
+    holders.on('dragover', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    holders.on('dragover', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    holders.on('drop', function (e) {
+        if (e.originalEvent.dataTransfer) {
+            if (e.originalEvent.dataTransfer.files.length) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                // TODO check for size of 1
+                var files = e.originalEvent.dataTransfer.files;
+
+                var dropTargetId = $(e.target).attr('id');
+                console.log("drop target id is " + dropTargetId);
+
+                // which holder?
+                var imgIdx = dropTargetId.substring('holder'.length);
+
+                console.log("idx: " + imgIdx + ", files: " + files);
+                for (var i = 0; i < files.length; ++i) {
+                    var filePath = files[i].path;
+                    console.log("Dropped file " + filePath);
+                    setImage(filePath, imgIdx);
+                    $("#dropped-file" + imgIdx).val(filePath);
+                }
+                return false;
+            }
+        }
+    });
+
     $("input:file").change(function () {
         var fileName = $(this).val();
         var fileNumber = $(this).target;
         console.log("Chose file " + fileName);
         setImage(fileName, 1);
-     });
-    
-    $("#make-button").click(function() {
-        console.log("Clicked make button");
+    });
+
+    $("#make-button").click(function () {
         var values = {};
-        $.each($('#params').serializeArray(), function(i, field) {
+        $.each($('#params').serializeArray(), function (i, field) {
             console.log("field: " + field);
             values[field.name] = field.value;
         });
         console.log(values);
+
+        // serializeArray does not work on file inputs
         var img1 = $('#file1').val();
+        if (img1 == "") {
+            img1 = $("#dropped-file1").val();
+        }
         var img2 = $('#file2').val();
-        
-        if (img1 == undefined || img1.length < 1) {
-            window.open("Please select a first image");
+        if (img2 == "") {
+            img2 = $("#dropped-file2").val();
         }
-        
-        if (img2 == undefined || img2.length < 1) {
-            window.open("Please select a second image");
+
+        if (!img1 || img1 == "") {
+            console.log("img1 not selected");
+            return;
         }
-        console.log("img1 : " + img1);
-        createDiptych(img1, img2, '', values=['color']);
+        if (!img2 || img2 == "") {
+            console.log("img2 not selected");
+            return;
+        }
+
+        createDiptych(img1, img2, '', values = ['color']);
+    });
+
+    $("#clear-button").click(function () {
+        console.log("clear button clicked");
+        $("#file1").val("");
+        $("#file2").val("");
+        $("#dropped-file1").val("");
+        $("#dropped-file2").val("");
+        $(".holder").empty();
     });
 });
-
-
